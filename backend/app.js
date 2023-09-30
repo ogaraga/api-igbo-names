1f301eef487afb23bd4497d6f7be48de96efcf81
Ogaraga <raymond.ogaraga@gmail.com>
2023-09-30 19:21:08 +0100

    review

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 //MIDDLEWARE
-app.use(cors({origin: "https://api-igbo-names.vercel.app/"}))
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', router);
--- backend/routes/nameRoutes.js
+++ backend/routes/nameRoutes.js
@@ -2,25 +2,31 @@ import express from 'express';
 import { yourNames } from '../allNames/igboNames.js';
 export const router = express();
 
-
-//getting route to work
-router.get('/api/igbo_names', async (req, res, next) => {
-    const {name} = req.body;
-    try {
-        const foundName = await yourNames.find(item=>item.name===name || item.name.toLowerCase()===name||item.name.toUpperCase()===name)
-    if (name ===foundName.name.toLowerCase() || name===foundName.name.toUpperCase()||name===foundName.name){
-      res.status(200).json({
-        Message:' Name found!',
-        NameInfo: foundName
-      });
-     }
-     else{
-      res.status(404).json('Name not found!')
-     } 
-  } catch (error) {
-    res.json('Something went wrong!')
-  }
-  next()
+//get restapi and get rote
+router.get('/api/igbo_names', async (req, res, next)=>{
+  const {name} = req.body;
+  try {
+    const foundName = await yourNames.find(item=>item.name===name || item.name.toLowerCase()===name||item.name.toUpperCase()===name)
+if (name ===foundName.name.toLowerCase() || name===foundName.name.toUpperCase()||name===foundName.name){
+  res.status(200).json({
+    Message: 'Name found!',
+    NameInfo: foundName
+  })
+}
+}catch(error){
+res.status(404).json(error.message)
+}
+  
+ next();
 });
+//   axios.get('http://localhost:4000/api/igbo_names',{
+//   headers:{
+//     "Content-Type":'application/json'
+//   },
+// }).then(resData => console.log(resData.data)).catch(err=>console.log(err))
+
+
+
+    
 
  
